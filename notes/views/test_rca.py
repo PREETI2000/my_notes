7a7c848b65c756a4bee95af3776e03b1b18d769d
preetibhandiwad <preeti.bhandiwad@pocketfm.com>
2024-10-03 21:11:22 +0530

    added other cases

--- notes/views/test_rca.py
+++ notes/views/test_rca.py
@@ -1,5 +1,14 @@
+from notes.models import NotesUser
+
+
 def test(data):
-    rewards = data.get("reward")
+    rewards = data["reward"]
     if rewards > 10:
         print("rewards is greater than 10")
+
     return True
+
+def test_empty_list():
+    note_user = NotesUser.objects.filter(name="preeti")
+    return note_user[0].name
+    
