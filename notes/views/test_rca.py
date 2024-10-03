e2010d10597deeec9f8e46689e869913232ac77a
preetibhandiwad <preeti.bhandiwad@pocketfm.com>
2024-10-03 23:12:31 +0530

    tetsing the instance value

--- notes/views/test_rca.py
+++ notes/views/test_rca.py
@@ -12,3 +12,9 @@ def test_empty_list():
     note_user = NotesUser.objects.filter(name="preeti")
     return note_user[0].name
     
+def test_keyerror(user_data):
+    rewards = user_data.get("days", None)
+    if rewards > 30:
+        print("user is eligible")
+        return True
+    return False
