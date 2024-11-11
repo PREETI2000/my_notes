9d4bc988c8388bb14287e626aafd6d5bb42980a4
preetibhandiwad <preeti.bhandiwad@pocketfm.com>
2024-11-11 09:45:26 +0530

    added correct instructions

--- notes/views/new_funct.py
+++ notes/views/new_funct.py
@@ -8,17 +8,19 @@ def get_qs_data_list(qs, data_key):
         qs = qs[0]
     return qs
 
-def get_story_text(story_info):
-    ds = get_qs_data_list(story_info, data=story_info)
-    if ds:
-        story_share_text = story_info.get('story_title') + "\n" + STORY_SHARE_MESSAGE_SUFFIX
-        print("note user exists")
-    x = True if ds else False
-    return x
 
 def test_empty_list():
     note_user = NotesUser.objects.filter(name="preeti")
     return note_user[0].name
+
+def get_story_text(story_info):
+    ds = get_qs_data_list(story_info, data=story_info)
+    if ds:
+        story_share_text = story_info.get('story_title') + "\n" + STORY_SHARE_MESSAGE_SUFFIX
+        print("story_share_text--", story_share_text)
+    x = True if ds else False
+    return x
+
 
 def test_keyerror(user_data):
     rewards = user_data.get("days", None)
